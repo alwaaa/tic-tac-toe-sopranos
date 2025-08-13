bf133550de70311460425101e1c26e063b5c8608
alwaaa <alwabusiness@gmail.com>
2025-08-13 16:47:43 +0500

    third commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Объект, в котором хранится состояние игры
 const state = {
     board: ["", "", "", "", "", "", "", "", ""],
     winConditions: [
@@ -13,9 +14,8 @@ const state = {
     currentPlayer: "X",
     moves: 0,
     gameOver: false,
-    resetButton: null,
 }
-
+// Объект, в котором хранятся DOM-элементы
 const elements = {
     cells: document.querySelectorAll(".cell"),
     field: document.querySelector("#field"),
@@ -23,14 +23,15 @@ const elements = {
     startGame: document.querySelector("#startGame"),
     mainContainer: document.querySelector("#mainContainer"),
     welcomeScreen: document.querySelector("#welcomeScreen"),
+    finalScreen: document.querySelector("#finalScreen"),
 }
-
+// Стартовое значение поля результата
 elements.result.textContent = `Ходит ${state.currentPlayer}`
-
+// Переключение игрока
 const switchPlayer = () => {
     state.currentPlayer = state.currentPlayer === "X" ? "O" : "X"
 }
-
+// Проверка победителя
 const checkWinner = () => {
     for (let [a, b, c] of state.winConditions) {
         if (
@@ -44,46 +45,52 @@ const checkWinner = () => {
     }
     return false
 }
-
+// Подсвечивание выигрышных клеток
 const highlightCells = (a, b, c) => {
     elements.cells[a].classList.add("winner")
     elements.cells[b].classList.add("winner")
     elements.cells[c].classList.add("winner")
 }
-
+// Проверка на ничью
 const checkDraw = () => {
     if (!state.gameOver && state.moves === 9) {
         return true
     }
     return false
 }
-
+// Основная функция хода
 const move = (input) => {
     if (state.board[input] !== "" || state.gameOver === true) return
     state.board[input] = state.currentPlayer
     elements.cells[input].textContent = state.currentPlayer
     state.moves++
-    console.log(state.board)
     if (checkWinner()) {
         state.gameOver = true
         elements.result.textContent = `Победил ${state.currentPlayer}`
         createResetButton()
+        setTimeout(() => {
+            finalScreen()
+        }, 1000)
     }
     if (checkDraw()) {
         state.gameOver = true
         elements.result.textContent = `Ничья`
         createResetButton()
+        setTimeout(() => {
+            finalScreen()
+        }, 1000)
     }
     if (!state.gameOver) {
         switchPlayer()
         elements.result.textContent = `Ходит ${state.currentPlayer}`
     }
 }
-
+// Функция кнопки сброса
 const createResetButton = () => {
     const resetButton = document.createElement("button")
+    resetButton.classList.add("resetButton")
     resetButton.textContent = "Новая игра"
-    elements.result.appendChild(resetButton)
+    elements.finalScreen.appendChild(resetButton)
     resetButton.addEventListener("click", () => {
         elements.cells.forEach((cell) => {
             cell.textContent = ""
@@ -93,16 +100,25 @@ const createResetButton = () => {
         state.gameOver = false
         state.moves = 0
         state.currentPlayer = "X"
+        elements.finalScreen.classList.add("hidden")
+        elements.mainContainer.classList.remove("transparent")
         elements.result.textContent = `Ходит ${state.currentPlayer}`
     })
 }
-
+// Функция финального окна
+const finalScreen = () => {
+    elements.finalScreen.classList.remove("hidden")
+    elements.mainContainer.classList.add("transparent")
+    elements.finalScreen.textContent = elements.result.textContent
+    createResetButton()
+}
+// Обработчик событий для клеток
 elements.cells.forEach((cell, index) => {
     cell.addEventListener("click", () => {
         move(index)
     })
 })
-
+// Обработчик событий для кнопки запуска новой игры
 elements.startGame.addEventListener("click", () => {
     elements.mainContainer.classList.remove("hidden")
     elements.welcomeScreen.classList.add("hidden")
